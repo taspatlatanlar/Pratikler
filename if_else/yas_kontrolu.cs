b6ac13c85d1ff3f1ab5f514f05244306fb800acf
Can Taspatlatanlar <can.taspatlatanlar@gmail.com>
2022-01-01 19:27:54 +0300

    Yaş kontrolü pratiği eklendi.

---
+++ if_else/yas_kontrolu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hoşgeldiniz, yaşınızı giriniz:");
+            int yas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girdiğiniz yaş: " + yas);
+
+            if(yas < 0){
+                Console.WriteLine("Yaşınız negatif olamaz!");
+            }
+            else if(yas >= 18){
+                Console.WriteLine("Mekana girebilirsiniz, iyi eğlenceler.");
+            }
+            else{
+                Console.WriteLine("Bu mekana girmek için çok gençsiniz!");
+            }
+        }
+    }
+}
